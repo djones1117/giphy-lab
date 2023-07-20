4ff198ecb573af000b64a893738c333050ba88db
djones1117 <socjones21@yahoo.com>
2023-07-20 15:49:37 -0400

    search pulls a list of gifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SearchGiphyForm from './SearchGiphyForm/SearchGiphyForm'
+import GiphInfo from './GiphInfo/GiphInfo'
 
 
 function App() {
@@ -10,12 +11,40 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('cheeseburger');
 const [giph, setGiph] = useState({})
 
-  const giphyUrl = `api.giphy.com/v1/gifs/search?api_key=Owou6qtq7DnLcxn4oaRdZGEdcZ5oM8RK&q=${searchTerm}`
 
+
+function getGiphySearch(giphTitle){
+  setSearchTerm(giphTitle)
+}
+
+
+useEffect(() => {
+
+const giphyUrl = `https://api.giphy.com/v1/gifs/search?api_key=Owou6qtq7DnLcxn4oaRdZGEdcZ5oM8RK&q=${searchTerm}`
+
+async function getGiphyInfo(){
+
+try {
+  const apiResponse = await fetch(giphyUrl);
+  
+  const data = await apiResponse.json();
+  console.log(data)
+
+setGiph(data)
+} catch(err){
+  console.log(err, 'error from api call')
+}
+
+}
+getGiphyInfo();
+
+}, [searchTerm])
   return (
     <>
-    <SearchGiphyForm/>
-     <p>The user is searching for {searchTerm}</p> 
+    <SearchGiphyForm getGiphySearch={getGiphySearch}/>
+     <p>The user is searching for {searchTerm}</p>
+     
+     {giph.Title ? <GiphInfo giph={giph}/> : null}
     </>
   )
 }
--- src/SearchGiphyForm/SearchGiphyForm.jsx
+++ src/SearchGiphyForm/SearchGiphyForm.jsx
@@ -1,21 +1,26 @@
-import { useState} from 'react'
-export default function SearchGiphyForm({getGiphySearch}){
-    const [giphyFormState, setGiphyFormState] = useState('');
-   
-   
-   function handleChange(g){
-    console.log(g.target.value)
+import { useState } from "react";
+export default function SearchGiphyForm({ getGiphySearch }) {
+  const [giphyFormState, setGiphyFormState] = useState("");
+
+  function handleChange(g) {
+    console.log(g.target.value);
     setGiphyFormState(g.target.value);
-   }
-   function handleSubmit(g){
+  }
+  function handleSubmit(g) {
     g.preventDefault();
     getGiphySearch(giphyFormState);
-    setGiphyFormState()
-   }
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Search Giphy" value={giphyFormState} onChange={handleChange} name='giphy'/>
-            <button>Search</button>
-        </form>
-    )
+   
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search Giphy"
+        value={giphyFormState}
+        onChange={handleChange}
+        name="giphy"
+      />
+      <button>Search</button>
+    </form>
+  );
 }
